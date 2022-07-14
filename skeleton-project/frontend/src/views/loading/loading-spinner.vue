de06c6934977af64dbd46eec4f2319213e768f9d
unknown <ghc0409@gmail.com>
2022-07-14 16:42:07 +0900

    rebase

---
+++ skeleton-project/frontend/src/views/loading/loading-spinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-loading='fullscreenloading'>
+      <p>로딩중입니다 잠시만 기다려 주세요</p>
+      <p>{{ fullscreenloading }}</p>
+      <!-- <p>잘 안보이지롱</p> -->
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const fullscreenloading = ref(true)
+</script>
+
+<style>
+
+</style>
---
+++ skeleton-project/frontend/src/views/main/store/actions.js
@@ -0,0 +1,16 @@
+// API
+import $axios from 'axios'
+
+export function requestLogin ({ state }, payload) {
+  console.log('requestLogin', state, payload)
+  const url = '/auth/login'
+  let body = payload
+  return $axios.post(url, body)
+}
+
+export function requestSignup ({ state }, payload) {
+  console.log('requestSignup', state, payload)
+  const url = '/users'
+  let body = payload
+  return $axios.post(url, body)
+}
